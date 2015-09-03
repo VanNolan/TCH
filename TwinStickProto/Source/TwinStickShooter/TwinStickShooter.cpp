28f4b885c378e720139c5330292e478b2f0b50df
Nicolas DEMAY <demay.nicolas@gmail.com>
2015-09-03 22:07:53 +0200

    Add TwinStickShooter Proto sources
    Only C++ sources et projects

---
+++ TwinStickProto/Source/TwinStickShooter/TwinStickShooter.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "TwinStickShooter.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwinStickShooter, "TwinStickShooter" );
+
+DEFINE_LOG_CATEGORY(LogTwinStickShooter)
+ 
