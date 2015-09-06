8559adce37246a7ef509c4808ede6856f8fc223a
Nicolas DEMAY <demay.nicolas@gmail.com>
2015-09-06 16:50:13 +0200

    Revert "Add TwinStickShooter Proto sources"
    This reverts commit 28f4b885c378e720139c5330292e478b2f0b50df.

--- TwinStickProto/Source/TwinStickShooter/TwinStickShooter.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "TwinStickShooter.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwinStickShooter, "TwinStickShooter" );
-
-DEFINE_LOG_CATEGORY(LogTwinStickShooter)
- 
