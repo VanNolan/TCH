72d72d248e9be4f8e62e3868f55f6587844de951
potron martial <potron.martial@gmail.com>
2015-09-09 00:35:42 +0200

    Add animation folder + branch new animation for aiming + all animation graphe En même temps, c'est mon premier commit, donc A LA GRACE DE DIEU, TOUT CA TOUT CA DESOLE SI CA CASSE !

---
+++ TwinStickShooter/Intermediate/Build/Win64/UE4Editor/Development/TwinStickShooter/PCH.TwinStickShooter.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\potron\Documents\TCH\TwinStickShooter\Source\TwinStickShooter\TwinStickShooter.h"
---
+++ TwinStickShooter/Intermediate/Build/Win64/UE4Editor/Development/TwinStickShooter/TwinStickShooter.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_TWINSTICKSHOOTER_H__
+#define __AUTO_TWINSTICKSHOOTER_H__
+//Last Write: 08/09/2015 21:50:01 +00:00
+#include "C:\Users\potron\Documents\TCH\TwinStickShooter\Source\TwinStickShooter\TwinStickShooter.h"
+#endif//__AUTO_TWINSTICKSHOOTER_H__
