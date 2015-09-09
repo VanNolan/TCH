d675744fd0baca31c58791f9f37dae51dc7fbdbc
VanNolan <yvan.laval@gmail.com>
2015-09-09 22:34:22 +0200

    Remove /Intermediate & /Saved files from repository

--- TwinStickShooter/Intermediate/Build/Win64/UE4Editor/Development/TwinStickShooter/TwinStickShooter.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_TWINSTICKSHOOTER_H__
-#define __AUTO_TWINSTICKSHOOTER_H__
-//Last Write: 08/09/2015 22:50:02 +00:00
-#include "C:\Users\potron\Documents\TCH\TwinStickShooter\Source\TwinStickShooter\TwinStickShooter.h"
-#endif//__AUTO_TWINSTICKSHOOTER_H__
