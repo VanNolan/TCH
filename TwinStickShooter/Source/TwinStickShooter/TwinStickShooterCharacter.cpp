9a19e98c96ac33d3f70783e6becaf3688d89c19d
VanNolan <yvan.laval@gmail.com>
2015-09-28 22:46:00 +0200

    Reworked all TwinStickShooterCharacter c++ code in blueprint > This will allow us to move some behavior in other blueprints (bullets and weapon behavior should be independant) + Fixed post effect bug on server

--- TwinStickShooter/Source/TwinStickShooter/TwinStickShooterCharacter.cpp
+++ TwinStickShooter/Source/TwinStickShooter/TwinStickShooterCharacter.cpp
@@ -41,14 +41,6 @@ ATwinStickShooterCharacter::ATwinStickShooterCharacter()
 	TwinStickShooterCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TwinStickShooterCamera"));
 	TwinStickShooterCameraComponent->AttachTo(CameraBoom, USpringArmComponent::SocketName);
 	TwinStickShooterCameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
-	// Movement
-	MoveSpeed = 1000.0f;
-	// Weapon
-	GunOffset = FVector(90.f, 0.f, 0.f);
-	FireRate = 0.1f;
-	bCanFire = true; 
-	CanShoot = true;
 }
 
 void ATwinStickShooterCharacter::SetupPlayerInputComponent(class UInputComponent* InputComponent)
@@ -65,109 +57,5 @@ void ATwinStickShooterCharacter::SetupPlayerInputComponent(class UInputComponent
 
 void ATwinStickShooterCharacter::Tick(float DeltaSeconds)
 {
-	// Find movement direction
-	const float ForwardValue = GetInputAxisValue(MoveForwardBinding);
-	const float RightValue = GetInputAxisValue(MoveRightBinding);
-
-	// Clamp max size so that (X=1, Y=1) doesn't cause faster movement in diagonal directions
-	const FVector MoveDirection = FVector(ForwardValue, RightValue, 0.f).GetClampedToMaxSize(1.0f);
-
-	// Calculate  movement
-	const FVector Movement = MoveDirection * MoveSpeed;
-	
-	// If non-zero size, move this actor
-	if (Movement.SizeSquared() > 0.0f)
-	{
-		AddMovementInput(Movement, 1.0f, false);
-	}
-
-
-	// Create fire direction vector
-	const float FireForwardValue = GetInputAxisValue(FireForwardBinding);
-	const float FireRightValue = GetInputAxisValue(FireRightBinding);
-	FireDirection = FVector(FireForwardValue, FireRightValue, 0.f);
-	// If we are pressing fire stick in a direction
-	if (FireDirection.SizeSquared() <= 0.000001f)
-		FireDirection = MoveDirection;
-	if (FireDirection.SizeSquared() <= 0.000001f)
-		FireDirection = GetActorForwardVector();
-
-	const float ZoomTriggerValue = GetInputAxisValue(ZoomTriggerBinding);
-	// Try and fire a shot
-	if (ZoomTriggerValue > 0.7f)
-	{
-		FireShot_Implementation(FireDirection);
-	}
-	
 	OnTick(DeltaSeconds);
 }
-
-void ATwinStickShooterCharacter::FireShot_Implementation(FVector FireDirection)
-{
-	if (!CanShoot)
-		return;
-
-	// If we it's ok to fire again
-	if (bCanFire == true)
-	{
-		FireShot(FireDirection);
-
-		const FRotator FireRotation = FireDirection.Rotation();
-		// Spawn projectile at an offset from this pawn
-		const FVector SpawnLocation = GetActorLocation() + FireRotation.RotateVector(GunOffset);
-
-		UWorld* const World = GetWorld();
-		if (World != NULL)
-		{
-			//Spawn only server side at it will automatically spawns a replicate on client's side
-			if (Role >= ROLE_Authority)
-			{
-				// spawn the projectile
-				if (ProjectileClass != NULL)
-				{
-					FActorSpawnParameters params;
-					params.Owner = this;
-					params.Instigator = this;
-					World->SpawnActor<AActor>(ProjectileClass, SpawnLocation, FireRotation, params);
-				}
-				//ATwinStickShooterProjectile* projectile = World->SpawnActor<ATwinStickShooterProjectile>(SpawnLocation, FireRotation);
-			}
-
-			World->GetTimerManager().SetTimer(TimerHandle_ShotTimerExpired, this, &ATwinStickShooterCharacter::ShotTimerExpired, FireRate);
-
-			// try and play the sound if specified
-			if (FireSound != nullptr)
-			{
-				UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
-			}
-		}
-
-		bCanFire = false; 
-	}
-
-	// If this next check succeeds, we are *not* the authority, meaning we are a network client.
-	// In this case we also want to call the server function to tell it to change the bSomeBool property as well.
-	if (Role < ROLE_Authority)
-	{
-		ServerFireShot(FireDirection);
-	}
-}
-
-bool ATwinStickShooterCharacter::ServerFireShot_Validate(FVector FireDirection)
-{
-	return true;
-}
-
-void ATwinStickShooterCharacter::ServerFireShot_Implementation(FVector FireDirection)
-{
-	// This function is only called on the server (where Role == ROLE_Authority), called over the network by clients.
-	// We need to call FireShot()!
-	// Inside that function, Role == ROLE_Authority, so it won't try to call ServerSetSomeBool() again.
-	FireShot_Implementation(FireDirection);
-}
-
-void ATwinStickShooterCharacter::ShotTimerExpired()
-{
-	bCanFire = true;
-}
-
--- TwinStickShooter/Source/TwinStickShooter/TwinStickShooterCharacter.h
+++ TwinStickShooter/Source/TwinStickShooter/TwinStickShooterCharacter.h
@@ -24,44 +24,12 @@ public:
 	/** Returns TCHeroesCameraComponent subobject **/
 	FORCEINLINE class UCameraComponent* GetTwinStickShooterCameraComponent() const { return TwinStickShooterCameraComponent; }
 
-	/** Get Fire Direction */
-	UFUNCTION(BlueprintPure, Category = "ATwinStickShooterPawn")
-	FVector GetFireDirection() const { return FireDirection; }
-
-
-	/** Offset from the ships location to spawn projectiles */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
-		FVector GunOffset;
-
-	/* How fast the weapon will fire */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
-		float FireRate;
-
-	/* The speed our ship moves around the level */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
-		float MoveSpeed;
-
-	/** Sound to play each time we fire */
-	UPROPERTY(Category = Audio, EditAnywhere, BlueprintReadWrite)
-	class USoundBase* FireSound;
-
 	// Begin Actor Interface
 	UFUNCTION(BlueprintImplementableEvent, Category = "Tick")
 	void OnTick(float DeltaSeconds);
 	virtual void Tick(float DeltaSeconds) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 	// End Actor Interface
-
-	/* Fire a shot in the specified direction */
-	UFUNCTION(BlueprintNativeEvent, Category = "Firing")
-	void FireShot(FVector FireDirection);
-	UFUNCTION(Server, WithValidation, Reliable)
-	void ServerFireShot(FVector FireDirection);
-	void ServerFireShot_Implementation(FVector FireDirection);
-	bool ServerFireShot_Validate(FVector FireDirection);
-
-	/* Handler for the fire timer expiry */
-	void ShotTimerExpired();
 
 	// Static names for axis bindings
 	static const FName MoveForwardBinding;
@@ -70,23 +38,8 @@ public:
 	static const FName FireRightBinding;
 	static const FName ZoomTriggerBinding;
 
-	/** Projectile actor to create each time the character shoots*/
-	UPROPERTY(Category = Bullet, EditAnywhere, BlueprintReadWrite)
-	UClass* ProjectileClass;
-
-	/** Projectile actor to create each time the character shoots*/
-	UPROPERTY(Category = Firing, EditAnywhere, BlueprintReadWrite)
-		bool CanShoot;
-
 private:
 
-	/* Flag to control firing  */
-	uint32 bCanFire : 1;
-
-	/** Handle for efficient management of ShotTimerExpired timer */
-	FTimerHandle TimerHandle_ShotTimerExpired;
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 
-	FVector FireDirection;
-	
 };
